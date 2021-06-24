5565de8dde3b79e9d91ab11518adedf978763781
Zakharov Ivan <zakharov_ii@proitr.ru>
2021-06-24 23:57:40 +0300

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String MyName;
+            MyName = "Jane";
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
