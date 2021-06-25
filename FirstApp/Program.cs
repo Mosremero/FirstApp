42a0663b37a7f8def2791bae420470c33af82c0f
Zakharov Ivan <mosremero@gmail.com>
2021-06-25 09:47:35 +0300

    Добавлено форматирование при выводе текста в консоль.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,11 +8,11 @@ namespace FirstApp
         {
             const string MyName = "Ivan";
 
-            Console.WriteLine(MyName);
+            Console.WriteLine(MyName + "\n");
 
-            Console.WriteLine("Привет, мир");
-            Console.WriteLine("Мне 32 года");
-            Console.WriteLine("My name is Ivan");
+            Console.WriteLine("\t  Привет, мир");
+            Console.WriteLine("\t  Мне 32 года");
+            Console.WriteLine("\t  My name is \n   Ivan");
 
             Console.ReadKey();
         }
