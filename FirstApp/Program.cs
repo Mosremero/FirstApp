44902839d20074f71d94381f5e4ba18e43f53286
Zakharov Ivan <mosremero@gmail.com>
2021-06-25 12:02:40 +0300

    Изменения в masterBranch по умолчанию

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,16 +6,7 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            const string MyName = "Ivan";
-
-            Console.WriteLine(MyName + "\n");
-
-            Console.WriteLine("\t  Привет, мир");
-            Console.WriteLine("\t  Мне 32 года");
-            Console.WriteLine("\t  My name is \n {0}", MyName);
-            Console.WriteLine("\u0040");
-            Console.WriteLine((char)35);
-            Console.ReadKey();
+            Console.WriteLine("FirstBranch");
         }
     }
 }
