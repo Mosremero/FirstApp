03519c82fbc731db87e3bbfb4093e04b16874922
Zakharov Ivan <mosremero@gmail.com>
2021-06-25 17:02:11 +0300

    Внесены измения согласно 3.7.4

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,7 +6,25 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("FirstBranch");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = (byte)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+            Console.Write("What is your favorite day of week? ");
+            daysOfWeek day = (daysOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your favorite day is {0}", day);
         }
+    }
+
+    enum daysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     }
 }
