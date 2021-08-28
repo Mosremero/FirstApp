d7a6b92d2d839fde7e61b18d88ea1830d96f0454
Zakharov Ivan <mosremero@gmail.com>
2021-08-28 21:43:03 +0300

    1

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,11 +6,12 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
+            (string name, int age) anketa;
             Console.Write("Как вас зовут: ");
-            var name = Console.ReadLine();
+            anketa.name = Console.ReadLine();
             Console.Write("Ваш возраст: ");
-            var age = Convert.ToByte(Console.ReadLine());
-            Console.WriteLine("Ваше имя {0} и ваш возраст {1}", name, age);
+            anketa.age = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Ваше имя {0} и ваш возраст {1}", anketa.name, anketa.age);
             Console.Write("Введите вашу дату рождения: ");
             var birthdate = Console.ReadLine();
             Console.Write("Ваша дата рождения {0}", birthdate);
